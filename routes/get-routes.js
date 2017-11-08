ce23cdcb249776b7e772fb5824d135761003bbd2
Mark Strelecky <streleck@gmail.com>
2017-11-08 13:41:20 -0600

    rewind

--- routes/get-routes.js
+++ routes/get-routes.js
@@ -6,8 +6,7 @@ var path = require("path");
 module.exports = function(app){
 	
 	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname,  '../public/', 'index.html'))
-	  // res.sendFile('public');
+	  res.sendFile('public');
 	});
 
 	app.get("/articles", function(req, res) {
