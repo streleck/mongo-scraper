f1f319d1052aa78caed39166eea7ba3e6764ce68
Mark Strelecky <streleck@gmail.com>
2017-11-08 14:17:57 -0600

    paths

--- routes/get-routes.js
+++ routes/get-routes.js
@@ -6,7 +6,7 @@ var path = require("path");
 module.exports = function(app){
 	
 	app.get("/", function(req, res) {
-	  res.sendFile('/app/public/index.html');
+	  res.sendFile('/app/Public/index.html');
 	});
 
 	app.get("/articles", function(req, res) {
